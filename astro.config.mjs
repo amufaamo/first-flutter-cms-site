8c36a075a5065357108353a9a97571d64db21184
Masakazu Ishikawa/石川昌和/いしかわまさかず <45814539+amufaamo@users.noreply.github.com>
2026-01-15 16:29:09 +0900

    Ensure astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com', // Will be overridden by GitHub Pages workflow usually or assumes relative
-  base: '/', 
+  site: 'https://amufaamo.github.io',
+  base: '/first-flutter-cms-site',
 });
